918ea6e377e857004f9752a027cf2bb6f9f5cdfc
Juan Manuel De La Torre Sanchez <72047760+JM-d3l4t0rr3@users.noreply.github.com>
2022-11-09 23:57:26 -0500

    changin ldap services

--- src/services/ldap.service.ts
+++ src/services/ldap.service.ts
@@ -3,7 +3,7 @@ import { authenticate } from 'ldap-authentication';
 async function ldapAuth(username: string, password: string) {
     const config = {
         ldapOpts: {
-            url: 'ldap://swarch2022ii-ldap:389',
+            url: 'ldap://swarch2022ii_ldap:389',
             tlsOptions: { rejectUnauthorized: false }
         },
         userDn: `cn=${username}@unal.edu.co,ou=sa,dc=arqsoft,dc=unal,dc=edu,dc=co`,
